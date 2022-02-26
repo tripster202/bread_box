994b108d1268df85c73c8b075cbb62b5281ce513
tripster202 <hockeyco28@gmail.com>
2022-02-26 13:08:29 -0600

    regular

--- hello_world/cpp/hello_world.cpp
+++ hello_world/cpp/hello_world.cpp
@@ -1,14 +1,17 @@
 // hello world
 
-# include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[])
+int main()
 {
-	/* code */
-	cout << "Hello World" << endl;
+	cout << "Hello World!";
 	return 0;
 }
 
 // compiler installation windows
 // https://code.visualstudio.com/docs/cpp/config-mingw
+// building and running cpp program
+// https://code.visualstudio.com/docs/languages/cpp
+// Neural Nine C++ YouTube Playlist
+// https://www.youtube.com/playlist?list=PL7yh-TELLS1HSmBcqYk9LhPYSWp31FWH2
